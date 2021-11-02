7dd2b3ff1d58d0d8b5b5fbb72b0dae414b70087f
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-11-02 20:10:37 -0300

    Added users as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-Rails.application.routes.draw do  
-  get "users", to: "users#index"
-  get "users/:id", to: "users#show"
-  get "users/:users_id/posts/:id", to: "posts#show"
-  get "users/:users_id/posts", to:"posts#index"
+Rails.application.routes.draw do
+  root 'users#index'
+  get 'users/:id', to: 'users#show'
+  get 'users/:users_id/posts/:id', to: 'posts#show'
+  get 'users/:users_id/posts', to: 'posts#index'
 end
