49450fd4f14691b066b73852d6abf57a2ed4ab24
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-11-04 18:41:14 -0300

    Corrected typo in integer

--- db/migrate/20211104211049_create_post.rb
+++ db/migrate/20211104211049_create_post.rb
@@ -3,7 +3,7 @@ class CreatePost < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.interger :comments_counter
+      t.integer :comments_counter
       t.integer :likes_counter
 
       t.timestamps
